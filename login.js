049b96659d060e6e399221141351dc5572865ced
PureGero <puregero@gmail.com>
2021-08-15 23:43:47 +1000

    Add ldap login

--- login.js
+++ login.js
@@ -2,4 +2,19 @@
  * Handle login requests
  */
 
-module.exports = (username, password) => 'staff';
+const config = require('./config.js');
+const ldap = require('ldapjs');
+
+module.exports = (username, password) => new Promise((resolve, reject) => {
+
+  const client = ldap.createClient({
+    url: `ldap://${config.domain}/`,
+    timeout: 5000,
+    connectTimeout: 10000
+  });
+
+  client.bind(username, password, error => {
+    resolve(!error);
+  });
+  
+});
